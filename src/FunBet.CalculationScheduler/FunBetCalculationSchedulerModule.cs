fdf4f48002f880cbf74cd242e9b4779c7b6b3a90
HOLE\hole <lelongho89@gmail.com>
2018-05-29 08:56:37 +0700

    add scheduler

---
+++ src/FunBet.CalculationScheduler/FunBetCalculationSchedulerModule.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunBet.CalculationScheduler
+{
+    [DependsOn(typeof(FunBetDataModule))]
+    public class FunBetCalculationSchedulerModule : AbpModule
+    {
+    }
+}
